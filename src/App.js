aa26786bd956efa9fc90100c4b777729ea9f6696
Betty Sosterics <betty.sosterics@gmail.com>
2023-09-11 15:31:51 +0200

    Install npm libraries

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
 export default function App() {
-  return <div></div>;
+  const backgroundColor = randomColor();
+  const hexCode = chalk.hex(backgroundColor);
+
+  return (
+    <>
+      <h1>React Random Color Generator</h1>
+      <button>Generate</button>
+      <div>
+        Generated Color: {backgroundColor} {hexCode}
+      </div>
+    </>
+  );
 }
